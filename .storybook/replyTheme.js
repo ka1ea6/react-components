5d9c228bccb1192c90813cc9ee4e7585bdd6f073
Rob Ellison <r.ellison@reply.com>
2024-12-29 00:34:25 +0000

    feat: add styles

---
+++ .storybook/replyTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Cortex Storybook',
+  brandUrl: 'https://cortexreply.com',
+  brandImage: '/cortex-reply.png',
+  brandTarget: '_self',
+});
