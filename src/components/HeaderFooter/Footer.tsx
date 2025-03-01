c70471ea5556d89159f5da18387ca0081f66e7c4
Rob Ellison <rob.ellison@r&r.org.uk>
2025-03-01 17:44:01 +0000

    fix: footer style

--- src/components/HeaderFooter/Footer.tsx
+++ src/components/HeaderFooter/Footer.tsx
@@ -69,7 +69,7 @@ export function Footer({ className, footerData, logoLight, logoDark }: SectionPr
   return (
     <footer
       className={cn(
-        'sticky z-2 flex flex-col section-padding-primary dark bg-background overflow-hidden ',
+        'sticky md:h-screen md:top-0 z-2 flex flex-col section-padding-primary dark bg-background overflow-hidden ',
         className,
       )}
     >
