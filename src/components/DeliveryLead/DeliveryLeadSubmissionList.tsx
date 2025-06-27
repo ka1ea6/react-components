0b1eb7de3e7f6a46ed65a4de6b0135392092b7ca
Fraser Steel <81355664+f-steel@users.noreply.github.com>
2025-06-27 15:13:35 +0100

    fix: delivery lead submissions

--- src/components/DeliveryLead/DeliveryLeadSubmissionList.tsx
+++ src/components/DeliveryLead/DeliveryLeadSubmissionList.tsx
@@ -215,7 +215,6 @@ export function DeliveryLeadSubmissionList({
               </button>
             </div>
 
-            {/* Modal Content */}
             <div className="p-6 space-y-6">
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 {/* Project Summary */}
